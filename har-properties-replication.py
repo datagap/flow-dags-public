912f26f795792882db47e44d4651845d7b121e58
Truong Nguyen <truong.nguyen@outlook.com>
2021-03-09 15:09:13 -0600

    full replication

---
+++ har-properties-replication.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.operators.http_operator import SimpleHttpOperator
+from airflow.utils.dates import days_ago
+
+DAG_NAME = 'har_properties_replication'
+
+args = {
+    'owner': 'datagap',
+}
+
+dag = DAG(
+    dag_id=DAG_NAME,
+    default_args=args,
+    start_date=days_ago(2),
+    schedule_interval='1 * * * *',
+    tags=['har']
+)
+
+t1 = SimpleHttpOperator(
+    task_id='post_op',
+    method='DELETE',
+    endpoint='http://druid-druid-cluster-routers.data.svc.cluster.local/druid­/in­dex­er/­v1/­tasks',
+    headers={"Content-Type": "application/json"},
+    response_check=lambda response: True if len(response.json()) == 0 else False,
+    dag=dag)
+
+t1
