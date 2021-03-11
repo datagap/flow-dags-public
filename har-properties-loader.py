12644407556fa9a94d65678f5f6adde7bad890ed
Truong Nguyen <truong.nguyen@outlook.com>
2021-03-11 12:07:40 -0600

    dag

--- har-properties-loader.py
+++ har-properties-loader.py
@@ -3,17 +3,11 @@ from datetime import datetime, timedelta
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.operators.dummy_operator import DummyOperator
 from kubernetes.client import models as k8s
+from airflow.utils.dates import days_ago
 
 default_args = {
     'owner': 'datagap'
 }
-
-dag = DAG(
-    'har-properties-loader', 
-    default_args=default_args, 
-    schedule_interval=None,
-    start_date=datetime.now()
-)
 
 volume = k8s.V1Volume(
     name='data-volume',
@@ -29,24 +23,30 @@ start = DummyOperator(task_id='start', dag=dag)
 # current year
 year = year = datetime.now().year
 
-# ingestion 10 years
-for i in range(11):
-    url = 'https://api.bridgedataoutput.com/api/v2/OData/har/Property/replication?access_token=c28535e677fb3fdf78253a99d3c5c1b2&$filter=year(ModificationTimestamp) eq {y}'.format(y=year)
-    
-    task = KubernetesPodOperator(namespace='data',
-                image="datagap/dataloader",
-                image_pull_policy='IfNotPresent',
-                cmds=["sh","-c", "dotnet DataLoader.dll '{link}' '/shared-data' 'har-2021'".format(link=url)],
-                annotations={'chaos.alpha.kubernetes.io/enabled': 'true'},
-                task_id="deploy-data-loader-pod-task-" + str(year),
-                name="har-properties-loader-pod-" + str(year),
-                volumes=[volume],
-                volume_mounts=[volume_mount],
-                get_logs=True,
-                dag=dag
-            )
+with DAG(
+    dag_id='har-properties-loader',
+    default_args=default_args,
+    schedule_interval=None,
+    start_date=days_ago(2),
+    tags=['har'],
+) as dag:
+    # ingestion 10 years
+    for i in range(11):
+        url = 'https://api.bridgedataoutput.com/api/v2/OData/har/Property/replication?access_token=c28535e677fb3fdf78253a99d3c5c1b2&$filter=year(ModificationTimestamp) eq {y}'.format(y=year)
+        
+        task = KubernetesPodOperator(namespace='data',
+                    image="datagap/dataloader",
+                    image_pull_policy='IfNotPresent',
+                    cmds=["sh","-c", "dotnet DataLoader.dll '{link}' '/shared-data' 'har-2021'".format(link=url)],
+                    annotations={'chaos.alpha.kubernetes.io/enabled': 'true'},
+                    task_id="deploy-data-loader-pod-task-" + str(year),
+                    name="har-properties-loader-pod-" + str(year),
+                    volumes=[volume],
+                    volume_mounts=[volume_mount],
+                    get_logs=True
+                )
 
-    start >> task
-    # another year
-    year = year - 1
+        start >> task
+        # another year
+        year = year - 1
     
