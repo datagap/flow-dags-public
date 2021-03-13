0b31703f5590fece8b9ca0751760284f9c088cdc
Truong Nguyen <truong.nguyen@outlook.com>
2021-03-13 13:47:43 -0600

    changed http post data format

--- har-property-full-index.py
+++ har-property-full-index.py
@@ -68,7 +68,7 @@ with DAG(
                 http_conn_id='druid-cluster',
                 endpoint='druid/indexer/v1/task',
                 headers={"Content-Type": "application/json"},
-                data=json.dumps(indexSpec),
+                data=indexSpec,
                 response_check=lambda response: True if "task" in response.content else False)
             )
 
